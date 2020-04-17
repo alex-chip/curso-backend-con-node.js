5aa5386e990b98daf5977629cad562844430ce6c
Alexander Chip <alexchip32@gmail.com>
2020-04-16 18:12:22 -0600

    Implementado capa de servicios

---
+++ movies-api/services/movies.js
@@ -0,0 +1,30 @@
+const { moviesMock } = require('../utils/mocks/movies');
+
+class MoviesService {
+  async getMovies() {
+    const routes = await Promise.resolve(moviesMock);
+    return routes || [];
+  }
+
+  async getMovie() {
+    const routes = await Promise.resolve(moviesMock[0]);
+    return routes || [];
+  }
+
+  async createMovie() {
+    const createMovieId = await Promise.resolve(moviesMock[0].id);
+    return createMovieId;
+  }
+
+  async deleteMovie() {
+    const deleteMovieId = await Promise.resolve(moviesMock[0].id);
+    return deleteMovieId;
+  }
+
+  async updateMovie() {
+    const updateMovieId = await Promise.resolve(moviesMock[0].id);
+    return updateMovieId;
+  }
+}
+
+module.exports = MoviesService;
